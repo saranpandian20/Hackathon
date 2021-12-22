e69e1df46a969f36652d97abce02e158238181fd
saranpandian20 <95117080+saranpandian20@users.noreply.github.com>
2021-12-22 16:41:49 +0530

    Update sample.java
    na vandhuten da selva

--- sample.java
+++ sample.java
@@ -1,5 +1,5 @@
 public class sample{
     public static void main(String[] args){
-        System.out.println("Hello selva");
+        System.out.println("bgm podu bgm podu...pam pam pam pam...");
     }
 }
